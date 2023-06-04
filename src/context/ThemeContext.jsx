d13dff1ec1cf2a00c15f0b14d765eabada126d4a
elcaneyvazli007 <104889259+elcaneyvazli007@users.noreply.github.com>
2023-06-04 20:40:36 +0400

    add context

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,18 +1,15 @@
-import { createContext,useState } from "react";
-
+import { createContext, useState } from "react";
 
 const ThemeContext = createContext();
 
 // eslint-disable-next-line react/prop-types
-function ThemeProvider({children}){
-    const [theme, setTheme] = useState("light");
-    return (
-        <ThemeContext.Provider value={{theme,setTheme}}>
-        {children}
-        </ThemeContext.Provider>
-    );
+function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
 
-
-
-export default {ThemeContext, ThemeProvider};
+export { ThemeContext, ThemeProvider };
